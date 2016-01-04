9e71bb2d4dea98c34c1136a8fffceee3847df42c
unknown <s130228@s130228.campus.tue.nl>
2016-01-04 11:25:38 +0100

    terrain

--- RobotRace/src/robotrace/Terrain.java
+++ RobotRace/src/robotrace/Terrain.java
@@ -20,12 +20,22 @@ class Terrain {
      * Draws the terrain.
      */
     public void draw(GL2 gl, GLU glu, GLUT glut) {
-        double dx = 0.5;
-        double dy = 0.5;
+        double dx = 1;
+        double dy = 1;
         for(double x = -20; x< 20; x+= dx){
             for(double y = -20; y< 20; y+= dy){
-                
-                
+                gl.glEnable(gl.GL_COLOR_MATERIAL);
+                gl.glColor3f(0.0f, 0.0f, 0.0f);
+                gl.glBegin(gl.GL_TRIANGLES);
+                   gl.glVertex3d(x, y+dy, heightAt(x, y+dy));
+                   gl.glVertex3d(x, y, heightAt(x, y));
+                   gl.glVertex3d(x+dx, y, heightAt(x+dx, y));
+                   
+                   gl.glVertex3d(x+dx, y+dy, heightAt(x+dx, y+dy));
+                   gl.glVertex3d(x+dx, y, heightAt(x+dx, y));
+                   gl.glVertex3d(x, y+dy, heightAt(x, y+dy));
+                gl.glEnd();
+                gl.glDisable(gl.GL_COLOR_MATERIAL);
             } 
         }
     }
@@ -33,9 +43,8 @@ class Terrain {
     /**
      * Computes the elevation of the terrain at (x, y).
      */
-    public float heightAt(float x, float y) {
+    public double heightAt(double x, double y) {
         double d = 0.6 * Math.cos(0.3 * x + 0.2 * y) + 0.4 * Math.cos(x - 0.5 * y);
-        float f = (float)d;
-        return f; // <- code goes here
+        return d; // <- code goes here
     }
 }
