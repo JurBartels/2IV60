be7e6979d5a02094fe1376d031aff42b864b0b6f
unknown <s130228@s130228.campus.tue.nl>
2016-01-11 17:33:51 +0100

    comments

--- RobotRace/src/robotrace/Camera.java
+++ RobotRace/src/robotrace/Camera.java
@@ -1,53 +1,63 @@
 package robotrace;
 
 import java.util.Random;
+
 /**
- * Implementation of a camera with a position and orientation. 
+ * Implementation of a camera with a position and orientation.
  */
 class Camera {
 
-    /** The position of the camera. */
+    /**
+     * The position of the camera.
+     */
     public Vector eye = new Vector(3f, 6f, 5f);
 
-    /** The point to which the camera is looking. */
+    /**
+     * The point to which the camera is looking.
+     */
     public Vector center = Vector.O;
 
-    /** The up vector. */
+    /**
+     * The up vector.
+     */
     public Vector up = Vector.Z;
-    
-    private int switchTime=5;
-    private double lastSwitch=0;
-    private int currentMode=1;
-    
+
+    //the time it takes for the automatic modus to switch from mode
+    private final int switchTime = 5;
+    //the time the last mode switch took place
+    private double lastSwitch = 0;
+    //the current mode the automode uses
+    private int currentMode = 1;
+
     private Random rand = new Random();
 
     /**
-     * Updates the camera viewpoint and direction based on the
-     * selected camera mode. test test 
+     * Updates the camera viewpoint and direction based on the selected camera
+     * mode. test test
      */
     public void update(GlobalState gs, Robot focus) {
         switch (gs.camMode) {
-            
+
             // Helicopter mode
             case 1:
                 setHelicopterMode(gs, focus);
                 break;
-                
+
             // Motor cycle mode    
             case 2:
                 setMotorCycleMode(gs, focus);
                 break;
-                
+
             // First person mode    
             case 3:
                 setFirstPersonMode(gs, focus);
                 break;
-                
+
             // Auto mode    
             case 4:
                 setAutoMode(gs, focus);
                 break;
-                
+
             // Default mode    
             default:
                 setDefaultMode(gs);
@@ -59,90 +69,91 @@ class Camera {
      */
     private void setDefaultMode(GlobalState gs) {
         //set camera coordinates based on spherical coordinates
-        eye.x = gs.vDist*Math.cos(gs.theta)*Math.sin(90-gs.phi);
-        eye.y = gs.vDist*Math.sin(gs.theta)*Math.sin(90-gs.phi);
-        eye.z = gs.vDist*Math.cos(90-gs.phi);
-        
+        eye.x = gs.vDist * Math.cos(gs.theta) * Math.sin(90 - gs.phi);
+        eye.y = gs.vDist * Math.sin(gs.theta) * Math.sin(90 - gs.phi);
+        eye.z = gs.vDist * Math.cos(90 - gs.phi);
+
         center = gs.cnt;
-        
+
         up = Vector.Z;
     }
 
     /**
-     * Computes eye, center, and up, based on the helicopter mode.
-     * The camera should focus on the robot.
+     * Computes eye, center, and up, based on the helicopter mode. The camera
+     * should focus on the robot.
      */
     private void setHelicopterMode(GlobalState gs, Robot focus) {
-            center = focus.pos;
-            up = focus.direction;
+        center = focus.pos;                                                     //the center point is the position of the robot that is focussed
+        up = focus.direction;                                                   //the up direction is the tangent of the robot that is focussed, such that the robot walks up on the screen
 
-            eye = center;
-            eye = eye.add(new Vector(0, 0, 20));
+        eye = center;                                                           //the x and y of the eye point are also at the robots position
+        eye = eye.add(new Vector(0, 0, 20));                                    //but the z is moved up by 20 such that we can look down to the center point
     }
 
     /**
-     * Computes eye, center, and up, based on the motorcycle mode.
-     * The camera should focus on the robot.
+     * Computes eye, center, and up, based on the motorcycle mode. The camera
+     * should focus on the robot.
      */
     private void setMotorCycleMode(GlobalState gs, Robot focus) {
-        center = focus.pos;
-        up = Vector.Z;
+        center = focus.pos;                                                     //the center point is the position of the robot that is focussed
+        up = Vector.Z;                                                          //the up direction is Vector.Z
 
         // calculate the eye position
-        eye = focus.direction.cross(Vector.Z).normalized().scale(10);
-        eye = center.add(eye);
-        eye = eye.add(new Vector(0, 0, 1));
+        eye = focus.direction.cross(up).normalized().scale(10);                 //the eye position is located at the robots position plus 10 meter outside the track
+        eye = center.add(eye);                                                  //to calculate in what direction this 10 meter is take the cross product from the up and the direction of the robot
+        eye = eye.add(new Vector(0, 0, 1));                                     //finally add 1 to the z axis in order to be at a 'motor level' compared to the robots
     }
 
     /**
-     * Computes eye, center, and up, based on the first person mode.
-     * The camera should view from the perspective of the robot.
+     * Computes eye, center, and up, based on the first person mode. The camera
+     * should view from the perspective of the robot.
      */
     private void setFirstPersonMode(GlobalState gs, Robot focus) {
-        eye = focus.pos;
-        eye = eye.add(new Vector(0, 0, 2));
-        eye = eye.add(focus.direction.normalized().scale(2));
-        up = Vector.Z;
+        eye = focus.pos;                                                        //the eye point is positioned at the robots head, so first take the robots position
+        eye = eye.add(new Vector(0, 0, 2));                                     //then add 2 to the z axis to be at the robots head
+        eye = eye.add(focus.direction.normalized().scale(0.5));                 //finally add .5 to be just in front of the head
+        up = Vector.Z;                                                          //the up vector is Vector.Z
 
         // center is in the direction of the tangent
-        center = focus.direction;
-        center.normalized().scale(10);
-        center = eye.add(center);
+        center = focus.direction;                                               //the center point is located in the direction the robot is running
+        center.normalized().scale(10);                                          //normalize this vector and then scale it to 10
+        center = eye.add(center);                                               //add the robots position to this vector
     }
-    
+
     /**
-     * Computes eye, center, and up, based on the auto mode.
-     * The above modes are alternated.
+     * Computes eye, center, and up, based on the auto mode. The above modes are
+     * alternated.
      */
     private void setAutoMode(GlobalState gs, Robot focus) {
-        if(gs.tAnim - lastSwitch > switchTime){
-            lastSwitch = gs.tAnim;
-            currentMode += 1;
-            if(currentMode > 3){
+        if (gs.tAnim - lastSwitch > switchTime) {                               //if the switchTime has passed
+            lastSwitch = gs.tAnim;                                              //then set the lastSwitch to the current time
+            currentMode += 1;                                                   //change the camera mode
+            if (currentMode > 3) {                                              //if the modes exceeds 3, then set it back to 1
                 currentMode = 1;
             }
         }
-        setCurrentMode(gs, focus, currentMode);
-        
+        setCurrentMode(gs, focus, currentMode);                                 //finally set the camera mode
+
     }
-    
-    private void setCurrentMode(GlobalState gs, Robot focus, int set){
-        switch(set){
+
+    //functino that setAutoMode to alternate between different modes 
+    private void setCurrentMode(GlobalState gs, Robot focus, int set) {
+        switch (set) {
             // Helicopter mode
             case 1:
                 setHelicopterMode(gs, focus);
                 break;
-                
+
             // Motor cycle mode    
             case 2:
                 setMotorCycleMode(gs, focus);
                 break;
-                
+
             // First person mode    
             case 3:
                 setFirstPersonMode(gs, focus);
                 break;
         }
     }
-    
+
 }
--- RobotRace/src/robotrace/RaceTrack.java
+++ RobotRace/src/robotrace/RaceTrack.java
@@ -38,121 +38,97 @@ class RaceTrack {
      */
     public void draw(GL2 gl, GLU glu, GLUT glut, GlobalState gs) {
         if (null == controlPoints) {
-
             //NOT using control points
             gl.glEnable(GL_COLOR_MATERIAL);
-            for (double j = -2; j < 3; j++) {
-                gl.glBegin(GL_LINE_LOOP);
-                for (double i = 0; i < 1; i = i + 0.02) {
-                    Vector a;
-                    a = getTangent(i).cross(new Vector(0, 0, 1));
-                    a.normalized();
-                    a.x = a.x * laneWidth;
-                    a.y = a.y * laneWidth;
-                    gl.glColor3f(1f, 0.0f, 0.0f);
-                    gl.glVertex3d(getPoint(i).x + j * a.x, getPoint(i).y + j * a.y, getPoint(i).z);
-                }
-                gl.glEnd();
-            }
             //Draw the lanes for the testtrack
-            gl.glColor3f(1f, 1f, 1f);
-            //gl.glEnable(GL_TEXTURE_2D);
-            RobotRace.track.enable(gl);
-            RobotRace.track.bind(gl);
-            gl.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+            gl.glColor3f(1f, 1f, 1f);                                           //set the color to white
+            RobotRace.track.enable(gl);                                         //enable the track texture
+            RobotRace.track.bind(gl);                                           //bind the track texture
+            gl.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);    //repeat the textures in both directions
             gl.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-            for (double j = -2; j < 2; j++) {
+            for (double j = -2; j < 2; j++) {                                   //lanes defined from centerline, -2, -1, 1, 2. at first this was easier since we defined the testtrack from the centerline two lanes inwards and two lanes outwards
                 gl.glBegin(GL_TRIANGLE_STRIP);
-                double pOffset = 0.01;
-                for (double i = 0; i <= 1 + 2*pOffset; i = i + 2 * pOffset) {
-
-                    Vector a;
-                    a = getTangent(i).cross(new Vector(0, 0, 1));
-                    a.normalized();
-                    a.x = a.x * laneWidth;
+                double pOffset = 0.01;                                          //set the offset that defines how many triangles are used
+                for (double i = 0; i <= 1 + 2*pOffset; i = i + 2 * pOffset) {                                                              
+                    Vector a = getTangent(i).cross(new Vector(0, 0, 1));        //vector a is orthogonal to the centerline, such that it can be multiplied by laneWidth to find the next lane
+                    a.normalized();                                             //normalize a, such that it can be multiplied by laneWidth
+                    a.x = a.x * laneWidth;                                      //multiply a by laneWidth
                     a.y = a.y * laneWidth;
-                    gl.glTexCoord2d(0, 0);
-                    gl.glVertex3d(getPoint(i).x + j * a.x, getPoint(i).y + j * a.y, getPoint(i).z);
-                    gl.glTexCoord2d(1, 0);
-                    gl.glVertex3d(getPoint(i).x + (j + 1) * a.x, getPoint(i).y + (j + 1) * a.y, getPoint(i).z);
+                    gl.glTexCoord2d(0, 0);                                                                              //set the first texture coordinate 
+                    gl.glVertex3d(getPoint(i).x + j * a.x, getPoint(i).y + j * a.y, getPoint(i).z);                     //set the first point of the triangle strip in the current lane (j)
+                    gl.glTexCoord2d(1, 0);                                                                              //set the second texture coordinate
+                    gl.glVertex3d(getPoint(i).x + (j + 1) * a.x, getPoint(i).y + (j + 1) * a.y, getPoint(i).z);         //set the second point of the strip in the next lane (j+1) (we add (j*a.x, j*a.y, 0) to find the point in lane j
 
-                    Vector b;
-                    b = getTangent(i + pOffset).cross(new Vector(0, 0, 1));
+                    Vector b;                                                   //now we do the same again with vector b, this is done because the texture map needs four coordinates, thats why we increment i by 2*offset instead of offset
+                    b = getTangent(i + pOffset).cross(new Vector(0, 0, 1));     //again b is orthogonal to the centerline such that we can find the correct lane
                     b.normalized();
-                    b.x = b.x * laneWidth;
+                    b.x = b.x * laneWidth;                                      //multiply by laneWidth    
                     b.y = b.y * laneWidth;
-                    gl.glTexCoord2d(0, 1);
-                    gl.glVertex3d(getPoint(i + pOffset).x + j * b.x, getPoint(i + pOffset).y + j * b.y, getPoint(i + pOffset).z);
-                    gl.glTexCoord2d(1, 1);
-                    gl.glVertex3d(getPoint(i + pOffset).x + (j + 1) * b.x, getPoint(i + pOffset).y + (j + 1) * b.y, getPoint(i + pOffset).z);
+                    gl.glTexCoord2d(0, 1);                                                                                                      //set the third texture coordinate
+                    gl.glVertex3d(getPoint(i + pOffset).x + j * b.x, getPoint(i + pOffset).y + j * b.y, getPoint(i + pOffset).z);               //do the same as above, but increment i
+                    gl.glTexCoord2d(1, 1);                                                                                                      //set the final coordinate for the texture
+                    gl.glVertex3d(getPoint(i + pOffset).x + (j + 1) * b.x, getPoint(i + pOffset).y + (j + 1) * b.y, getPoint(i + pOffset).z);   //do the same as above, but increment i
                 }
-                gl.glEnd();
+                gl.glEnd();                                                     //finish the triangle strip
             }
-            RobotRace.track.disable(gl);
+            RobotRace.track.disable(gl);                                        //disable the track texture
 
             gl.glColor3f(1f, 1f, 1f);
-            RobotRace.brick.enable(gl);
-            RobotRace.brick.bind(gl);
-            gl.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+            RobotRace.brick.enable(gl);                                         //enable the brick texture
+            RobotRace.brick.bind(gl);                                           //bind the brick texture
+            gl.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);    //repeat the texture in both directions
             gl.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
             //Inner circle that is located at -2*1.22 from the center line and outer circle that is located 2*1.22 from the centerline
-            for (int j = -2; j <= 2; j += 4) {
-                gl.glBegin(GL_QUADS);
-                double pOffset = 0.01;
-                for (double i = 0; i < 1; i = i + pOffset) {
-
-                    Vector a;
-                    a = getTangent(i).cross(new Vector(0, 0, 1));
+            for (int j = -2; j <= 2; j += 4) {                                  //only use lane curves -2 and 2, these represent the outside lanes
+                gl.glBegin(GL_QUADS);                                           //this time quads are used, since the 4 points lay in the same plane
+                double pOffset = 0.01;                                          //set the offset, this defines how many quads are used
+                for (double i = 0; i < 1; i = i + pOffset) {                    
+                    Vector a = getTangent(i).cross(new Vector(0, 0, 1));        //as with the drawing of the tracks above, use vector a to later on find the correct lane
                     a.normalized();
                     a.x = a.x * laneWidth;
                     a.y = a.y * laneWidth;
-                    gl.glTexCoord2d(0, 0);
-                    gl.glVertex3d(getPoint(i).x + j * a.x, getPoint(i).y + j * a.y, getPoint(i).z - 1);
-                    gl.glTexCoord2d(0, 1);
-                    gl.glVertex3d(getPoint(i).x + j * a.x, getPoint(i).y + j * a.y, getPoint(i).z);
+                    gl.glTexCoord2d(0, 0);                                                                              //set the first texture coordinate
+                    gl.glVertex3d(getPoint(i).x + j * a.x, getPoint(i).y + j * a.y, getPoint(i).z - 1);                 //set the first vertex, located at z-1
+                    gl.glTexCoord2d(0, 1);                                                                              //set the second texture coordinate
+                    gl.glVertex3d(getPoint(i).x + j * a.x, getPoint(i).y + j * a.y, getPoint(i).z);                     //set the second vertex, located at z
 
-                    Vector b;
-                    b = getTangent(i + pOffset).cross(new Vector(0, 0, 1));
+                    Vector b = getTangent(i + pOffset).cross(new Vector(0, 0, 1));  //as with the drawing of the tracks use vector b to find the correct lane, this time increment i again
                     b.normalized();
                     b.x = b.x * laneWidth;
                     b.y = b.y * laneWidth;
-                    gl.glTexCoord2d(1, 1);
-                    gl.glVertex3d(getPoint(i + pOffset).x + j * b.x, getPoint(i + pOffset).y + j * b.y, getPoint(i + pOffset).z);
-                    gl.glTexCoord2d(1, 0);
-                    gl.glVertex3d(getPoint(i + pOffset).x + j * b.x, getPoint(i + pOffset).y + j * b.y, getPoint(i + pOffset).z - 1);
+                    gl.glTexCoord2d(1, 1);                                                                                              //set the texture coordinate
+                    gl.glVertex3d(getPoint(i + pOffset).x + j * b.x, getPoint(i + pOffset).y + j * b.y, getPoint(i + pOffset).z);       //set the vertex, at point i with offset and z
+                    gl.glTexCoord2d(1, 0);                                                                                              //set texture coordinate
+                    gl.glVertex3d(getPoint(i + pOffset).x + j * b.x, getPoint(i + pOffset).y + j * b.y, getPoint(i + pOffset).z - 1);   //set final vertex at point i with offset and z-1
                 }
+                gl.glEnd();                                                     //finish the quad
             }
-            gl.glEnd();
-            RobotRace.brick.disable(gl);
+            RobotRace.brick.disable(gl);                                        //disable the brick texture                                  
             gl.glDisable(GL_COLOR_MATERIAL);
-        } else {
+        } else {                                                                //in this case there are controllpoints, so this is not the test track
             gl.glEnable(GL_COLOR_MATERIAL);
             gl.glColor3f(1f, 1f, 1f);
-            RobotRace.track.enable(gl);
-            RobotRace.track.bind(gl);
-            gl.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+            RobotRace.track.enable(gl);                                         //enable the track texture
+            RobotRace.track.bind(gl);                                           //bind the track texture
+            gl.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);    //repeat the texture in both directions
             gl.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
             // draw the spline track
-            for (int curve = 0; curve < 4; curve++) {
+            for (int lane = 0; lane < 4; lane++) {                           //at this point we decided to define the lanes 0 to 3 for easier coding
 
-                // Use a triangle strip and create a closed ring out of triangles
-                gl.glBegin(GL_TRIANGLE_STRIP);
-                // Normal is pointing up for track
-                gl.glNormal3d(0, 0, 1);
-                double pOffset = 0.01;
+                gl.glBegin(GL_TRIANGLE_STRIP);                                  //again use a triangle strip for the tracks
+                gl.glNormal3d(0, 0, 1);                                         //set the normal pointing upwards
+                double pOffset = 0.01;                                          //define an offset that determines how many triangles are drawn
                 for (double i = 0; i < 1; i = i + 2*pOffset) {
-                    // SEGMENTS times: add a vertex describing an inner and outer point of this curve
-                    //double t = (i-1) / ((double) 300);
-                    Vector inner = getPointOnCurve(i, curve,false);
-                    Vector outer = getPointOnCurve(i, curve + 1,false);
-                    // Add these two vectors, that are on the same distance on the track, as vertices to the triangle strip
+                    Vector inner = getPointOnCurve(i, lane,false);             //find the vertex at i in the first lane  
+                    Vector outer = getPointOnCurve(i, lane + 1,false);
+
                     gl.glTexCoord2d(0,0);
                     gl.glVertex3d(inner.x(), inner.y(), inner.z());
                     gl.glTexCoord2d(1,0);
                     gl.glVertex3d(outer.x(), outer.y(), outer.z());
 
-                    Vector inner2 = getPointOnCurve(i+pOffset, curve,false);
-                    Vector outer2 = getPointOnCurve(i+pOffset, curve + 1,false);
+                    Vector inner2 = getPointOnCurve(i+pOffset, lane,false);
+                    Vector outer2 = getPointOnCurve(i+pOffset, lane + 1,false);
 
                     gl.glTexCoord2d(0,1);
                     gl.glVertex3d(inner2.x(), inner2.y(), inner2.z());
@@ -160,15 +136,15 @@ class RaceTrack {
                     gl.glVertex3d(outer2.x(), outer2.y(), outer2.z());
                 }
                 //add last and first point to finish the triangle strip
-                Vector inner = getPointOnCurve(.99, curve,false);
-                Vector outer = getPointOnCurve(.99, curve + 1,false);
+                Vector inner = getPointOnCurve(.99, lane,false);
+                Vector outer = getPointOnCurve(.99, lane + 1,false);
                 gl.glTexCoord2d(0,0);
                 gl.glVertex3d(inner.x(), inner.y(), inner.z());
                 gl.glTexCoord2d(1,0);
                 gl.glVertex3d(outer.x(), outer.y(), outer.z());
                 
-                Vector inner2 = getPointOnCurve(0, curve,false);
-                Vector outer2 = getPointOnCurve(0, curve + 1,false);
+                Vector inner2 = getPointOnCurve(0, lane,false);
+                Vector outer2 = getPointOnCurve(0, lane + 1,false);
                 gl.glTexCoord2d(0,1);
                 gl.glVertex3d(inner2.x(), inner2.y(), inner2.z());
                 gl.glTexCoord2d(1,1);
@@ -368,7 +344,6 @@ class RaceTrack {
         double bezierT = (t - (((double) segment) / numberOfSegments)) * numberOfSegments;
         Vector point = getCubicBezierPoint(bezierT, P0, P1, P2, P3);
         Vector tangent = getCubicBezierTangent(bezierT, P0, P1, P2, P3).scale(-1);
-        //Vector tangent = getCubicBezierTangent(bezierT, P0, P1, P2, P3);
         Vector normal = tangent.cross(Vector.Z).normalized();
 
         if(robot){
